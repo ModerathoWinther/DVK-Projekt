e3a426dca3c35d83f069606e437b9b0f955dbef8
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-03-31 15:31:18 +0200

    ADD testing macd

---
+++ test_mt5_env.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+import MetaTrader5 as mt5
+import pandas as pd
+import indicators as ind
+
+if not mt5.initialize():
+    print("initialize() failed")
+    mt5.shutdown()
+
+print("terminal info: ", mt5.terminal_info())
+print("version: ", mt5.version())
+
+xauusd_ticks = mt5.copy_rates_from("XAUUSD", mt5.TIMEFRAME_H1, datetime(2020, 1, 10), 100)
+df = pd.DataFrame(xauusd_ticks)
+ind.macd(df)
+
+print(df)
+
+mt5.shutdown()

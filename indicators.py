b91c0e83eb5af13130e5e3be1926351f5498d4d5
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-03-31 15:50:24 +0200

    ADD atr function

--- indicators.py
+++ indicators.py
@@ -1,9 +1,18 @@
-import talib as ta
+import talib
 
+
+def atr(dataframe):
+    dataframe['atr'] = talib.ATR(
+        dataframe['high'],
+        dataframe['low'],
+        dataframe['close'])
 
 def macd(dataframe):
-    dataframe['macd'], dataframe['macd_signal'], dataframe['macd_histogram'] = ta.MACD(
+    dataframe['macd'], dataframe['macd_signal'], dataframe['macd_histogram'] = talib.MACD(
         dataframe['close'],
         fastperiod=12,
         slowperiod=26,
         signalperiod=9)
+
+def rsi(dataframe):
+    return 0

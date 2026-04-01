2322b9eba8e2ddde6bb130c4a12987fdf548aa60
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-04-02 01:08:01 +0200

    [REFACTOR] get_inputs

--- data_process.py
+++ data_process.py
@@ -11,22 +11,18 @@ SYMBOL      = "XAUUSD"
 
 def get_inputs(split, atr=False, macd=False, rsi=False):
     price = pd.read_csv(f"{INPUT_DIR}/{split}/raw_{split}.csv",
-                     index_col="date",
-                     parse_dates=["date"])
+                     index_col="date", parse_dates=["date"])
 
     frames = [price]
     if atr:
         frames.append(pd.read_csv(f"{OUTPUT_DIR}/{split}/atr.csv",
-                                      index_col="date",
-                                      parse_dates=["date"]))
+                                      index_col="date", parse_dates=["date"]))
     if macd:
         frames.append(pd.read_csv(f"{OUTPUT_DIR}/{split}/macd.csv",
-                                      index_col="date",
-                                      parse_dates=["date"]))
+                                      index_col="date", parse_dates=["date"]))
     if rsi:
         frames.append(pd.read_csv(f"{OUTPUT_DIR}/{split}/rsi.csv",
-                                      index_col="date",
-                                      parse_dates=["date"]))
+                                      index_col="date", parse_dates=["date"]))
     return pd.concat(frames, axis=1).dropna()
 
 

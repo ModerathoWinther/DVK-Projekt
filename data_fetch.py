477ab821276ef34eb5071400dcb5a0f27db7416b
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-04-01 18:29:36 +0200

    [REFACTOR] changed file path structures etc.

--- data_fetch.py
+++ data_fetch.py
@@ -5,29 +5,29 @@ from datetime import datetime
 import time
 import os
 
+TIMEFRAME = mt5.TIMEFRAME_M15
+SYMBOL = "XAUUSD"
+OUTPUT_DIR = "data/raw"
+TIMEZONE = pytz.timezone("Etc/UTC")
+DATE_FROM = datetime(2016, 1, 1, tzinfo=TIMEZONE)
+DATE_TO = datetime(2025, 12, 31, hour=23, tzinfo=TIMEZONE)
 
-def fetch_and_save_data(symbol="XAUUSD", timeframe=mt5.TIMEFRAME_M15):
+def fetch_and_save_data():
 
     if not mt5.initialize():
         print(f"MT5 initialize() failed, error code = {mt5.last_error()}")
         return
 
-    selected = mt5.symbol_select(symbol, True)
+    selected = mt5.symbol_select(SYMBOL, True)
     if not selected:
-        print(f"Failed to select {symbol}")
+        print(f"Failed to select {SYMBOL}")
         mt5.shutdown()
         return
 
-    print(f"Synchronizing history for {symbol}...")
+    print(f"Synchronizing history for {SYMBOL}...")
     time.sleep(2)
 
-
-    timezone = pytz.timezone("Etc/UTC")
-
-    date_from = datetime(2016, 1, 1, tzinfo=timezone)
-    date_to = datetime(2025, 12, 31, hour=23, tzinfo=timezone)
-
-    rates = mt5.copy_rates_range(symbol, timeframe, date_from, date_to)
+    rates = mt5.copy_rates_range(SYMBOL, TIMEFRAME, DATE_FROM, DATE_TO)
 
     if rates is None or len(rates) == 0:
         print(f"Error: No data retrieved. Error code: {mt5.last_error()}")
@@ -39,15 +39,14 @@ def fetch_and_save_data(symbol="XAUUSD", timeframe=mt5.TIMEFRAME_M15):
     df = pd.DataFrame(rates)
     df['time'] = pd.to_datetime(df['time'], unit='s')
     df = df.rename(columns={'time': 'date', 'tick_volume': 'volume'})
-    df['tick'] = symbol
+    df['tick'] = SYMBOL
 
     final_df = df[['date', 'open', 'high', 'low', 'close', 'volume', 'tick']]
 
-    if not os.path.exists('data/raw'):
-        os.makedirs('data/raw')
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     actual_rows = len(final_df)
-    fn = f"data/raw/{symbol}_{timeframe}M_{date_from.date()}_{date_to.date()}.csv"
+    fn = f"data/raw/{SYMBOL}_{TIMEFRAME}M_{DATE_FROM.date()}_{DATE_TO.date()}.csv"
 
     final_df.to_csv(fn, index=False)
     print(f"Successfully saved {actual_rows} rows to {fn}")
@@ -56,11 +55,11 @@ def fetch_and_save_data(symbol="XAUUSD", timeframe=mt5.TIMEFRAME_M15):
 
 def partition_fetched_data():
 
-    if not os.path.isfile(file_name):
-        print(f"Error: Raw data file not found with file name {file_name}.")
+    if not os.path.isfile(FILE_NAME):
+        print(f"Error: Raw data file not found with file name {FILE_NAME}.")
         return
 
-    df = pd.read_csv(file_name, parse_dates=['date'])
+    df = pd.read_csv(FILE_NAME, parse_dates=['date'])
     df = df.sort_values('date').reset_index(drop=True)
 
     train_end = pd.Timestamp('2022-12-31 23:59:59')
@@ -71,18 +70,21 @@ def partition_fetched_data():
     val_df   = df[(df['date'] > train_end) & (df['date'] <= val_end)].copy()
     test_df  = df[df['date'] > val_end].copy()
 
-    processed_dir = 'data/processed'
-    os.makedirs(processed_dir, exist_ok=True)
+    splits = ["train", "val", "test"]
+    for split in splits:
+        dirs = os.path.join(OUTPUT_DIR, split)
+        os.makedirs(dirs, exist_ok=True)
 
-    train_path = f"{processed_dir}/train.csv"
-    val_path   = f"{processed_dir}/validation.csv"
-    test_path  = f"{processed_dir}/test.csv"
+
+    train_path = f"{OUTPUT_DIR}/train/raw_train.csv"
+    val_path   = f"{OUTPUT_DIR}/val/raw_val.csv"
+    test_path  = f"{OUTPUT_DIR}/test/raw_test.csv"
     train_df.to_csv(train_path, index=False)
     val_df.to_csv(val_path, index=False)
     test_df.to_csv(test_path, index=False)
 
-    print(f"Saved splits to {processed_dir}/")
+    print(f"Saved splits to {OUTPUT_DIR}/X_path")
     return train_path, val_path, test_path
 
-file_name = fetch_and_save_data("XAUUSD", mt5.TIMEFRAME_M15)
+FILE_NAME = fetch_and_save_data()
 partition_fetched_data()

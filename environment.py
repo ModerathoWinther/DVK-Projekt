9b2f1654040d9e9f65e793844662bc786204b641
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-04-07 18:30:56 +0200

    [ADD] primitive reward system

--- environment.py
+++ environment.py
@@ -1,13 +1,18 @@
-import numpy
 from enum import Enum
+
+import numpy
 
 import init_state
 
-
+ENTRY_PER_TRADE = 4
 ACTION_INDEX = 0
 PRICE_INDEX = 1
+SL_INDEX = 2
+TP_INDEX = 3
 
-ENTRY_PER_TRADE = 2
+MARKET_HIGH = 1
+MARKET_LOW = 2
+MARKET_CLOSE = 3
 
 
 class Action(Enum):
@@ -45,19 +50,79 @@ class Environment:
                         break
 
                 self.trades[empty_trade + ACTION_INDEX] = action.value
-                self.trades[empty_trade + PRICE_INDEX] = state[3]
+                self.trades[empty_trade + PRICE_INDEX] = state[MARKET_CLOSE]
+                self.trades[empty_trade + SL_INDEX] = 0
+                self.trades[empty_trade + TP_INDEX] = 1065
                 self.open_slots -= 1
                 return self.get_current_state()
+
+    def get_reward_and_clear_trades(self):
+        current_md = self.market_data[self.index]
+        high = current_md[MARKET_HIGH]
+        low = current_md[MARKET_LOW]
+
+        sum_reward = 0
+        for i in range(self.num_trades):
+            trade = i * ENTRY_PER_TRADE
+            reward, closed = self.__calculate_reward(high, low, trade)
+            sum_reward += reward
+            if closed:
+                self.__set_trade_info(trade, 0, 0, 0, 0)
+
+        return sum_reward
 
     def can_trade(self):
         return self.open_slots > 0
 
+    def __get_trade_info(self, start_index):
+        action = self.trades[start_index + ACTION_INDEX]
+        price = self.trades[start_index + PRICE_INDEX]
+        sl = self.trades[start_index + SL_INDEX]
+        tp = self.trades[start_index + TP_INDEX]
+        return action, price, sl, tp
+
+    def __set_trade_info(self, start_index, action, price, sl, tp):
+        self.trades[start_index + ACTION_INDEX] = action
+        self.trades[start_index + PRICE_INDEX] = price
+        self.trades[start_index + SL_INDEX] = sl
+        self.trades[start_index + TP_INDEX] = tp
+
+    # todo Calculate reward using Sharpe ratio
+    def __calculate_reward(self, high, low, trade):
+        action, price, sl, tp = self.__get_trade_info(trade)
+        print(action, price, sl, tp)
+        closed = False
+        reward = 0
+        match action:
+            case Action.SELL.value:
+                if sl > high:
+                    reward = price - sl
+                    closed = True
+                elif tp < low:
+                    reward = price - tp
+                    closed = True
+            case Action.BUY.value:
+                if sl > low:
+                    reward = sl - price
+                    closed = True
+                if tp < high:
+                    reward = tp - price
+                    closed = True
+        print(reward, closed)
+        return reward, closed
+
 
 if __name__ == "__main__":
-    env = Environment("test", 2, atr=True, macd=True, rsi=True)
-    print(env.get_current_state())
-    print(env.perform_action(Action.HOLD))
-    print(env.perform_action(Action.SELL))
-    print(env.perform_action(Action.BUY))
+    env = Environment("train", 2)
+    print("reward:  ", env.get_reward_and_clear_trades())
+    env.perform_action(Action.HOLD)
+    print("reward:  ", env.get_reward_and_clear_trades())
+    env.perform_action(Action.BUY)
+    print("reward:  ", env.get_reward_and_clear_trades())
+    for i in range(10):
+        env.perform_action(Action.HOLD)
+        print("reward:  ", env.get_reward_and_clear_trades())
+
     # Should throw an Exception because too many trades
+    # print(env.perform_action(Action.BUY))
     # print(env.perform_action(action.BUY))

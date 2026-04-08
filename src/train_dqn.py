8110819b5f3683c791bb123d2da0be0f2cb5acb5
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-04-08 17:56:34 +0200

    [ADD] training loop structure

--- src/train_dqn.py
+++ src/train_dqn.py
@@ -1,11 +1,15 @@
-import tensorflow as tf
+from random import random, randrange
 
-import init_state
+import tensorflow as tf
+from environment import Environment
+from src.action_space import ACTION_SPACE
 
 NUM_INPUTS = 4
 NUM_OUTPUTS = 7
 
+EXPLORATION = 0.05
 LEARNING_RATE = 0.001
+DISCOUNT = 0.99
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
 loss_object = tf.keras.losses.MeanSquaredError()
@@ -24,29 +28,39 @@ def loss(model, x, y_true):
     return loss_object(y_true=y_true, y_pred=y_pred)
 
 
-#inputs = state, targets = true reward + future Q
+# inputs = state, targets = true reward + future Q
 def grad(model, inputs, targets):
     with tf.GradientTape() as tape:
         loss_value = loss(model, inputs, targets)
     return loss_value, tape.gradient(loss_value, model.trainable_variables)
 
 
-if __name__ == "__main__":
-    market_data, trades = init_state.run("test", 5, atr=True)
-    print(market_data)
-    print(trades)
+def train(atr=False, macd=False, rsi=False):
+    env = Environment("train", 5, atr=atr, macd=macd, rsi=rsi)
+    q_network = build_model()
+    target_network = build_model()
+    replay_buffer = []
+
+    while env.has_next():
+        state = env.get_current_state()
+        if random() <= EXPLORATION:
+            action_index = randrange(len(ACTION_SPACE))
+        else:
+            action_index = q_network(state)
+
+        action = ACTION_SPACE[action_index]
+        env.perform_action(action)
+        reward = env.get_reward_and_clear_trades()
+        new_state = env.get_current_state()
+
+        # todo store transition object in replay buffer
+
+        # todo sample random transition from replay buffer and apply gradient to q-network using target_network as reference
+
+        target_network = q_network
 
 
 """
-q_network = build_model()
-target_network = build_model()
-
-loss_value, grads = grad(target_network, )
-print("Step: {}, Initial Loss: {}".format(optimizer.iterations.numpy(),
-                                          loss_value.numpy()))
-
+loss_value, grads = grad(target_network, state, reward)
 optimizer.apply_gradients(zip(grads, q_network.trainable_variables))
 """
-
-
-

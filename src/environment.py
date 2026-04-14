0e4bfa7771df445f4be1515221e402fa80ae5968
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-04-14 02:59:17 +0200

    [ADD] get winrate, expectancy and profit_factor in backtests

--- src/environment.py
+++ src/environment.py
@@ -189,40 +189,58 @@ class BackTestEnvironment:
 
         high, low = env.__get_high_low()
 
-        for i in range(env.num_trades):
-            action, price, sl, tp = env.__get_trade_info(i)
+        for trade in range(env.num_trades):
+            action, price, sl, tp = env.__get_trade_info(trade)
 
             to_clear = []
             match action:
                 case Direction.SELL.value:
                     if sl < high:
-                        loss = price - sl
-                        self.total_profit += loss
-                        self.total_loss -= loss
-                        self.num_loss += 1
-                        to_clear.append(action)
+                        profit = price - sl
+                        self.__record_loss(profit * -1)
+                        to_clear.append(trade)
                     elif tp > low:
-                        gain = price - tp
-                        self.total_profit += gain
-                        self.total_gain += gain
-                        self.num_gain += 1
-                        to_clear.append(action)
+                        profit = price - tp
+                        self.__record_gain(profit)
+                        to_clear.append(trade)
                 case Direction.BUY.value:
                     if sl > low:
-                        loss = sl - price
-                        self.total_profit += loss
-                        self.total_loss -= loss
-                        self.num_loss = sl - price
-                        to_clear.append(action)
+                        profit = sl - price
+                        self.__record_loss(profit * -1)
+                        to_clear.append(trade)
                     elif tp < high:
-                        gain = tp - price
-                        self.total_profit += gain
-                        self.total_gain += gain
-                        self.num_gain += 1
-                        to_clear.append(action)
+                        profit = tp - price
+                        self.__record_gain(profit)
+                        to_clear.append(trade)
 
-            env.__clear(to_clear)
-            
+        env.__clear(to_clear)
+
+    def __record_loss(self, loss):
+        self.closed_trades += 1
+        self.total_profit -= loss
+        self.total_loss += loss
+        self.num_loss += 1
+
+    def __record_gain(self, gain):
+        self.closed_trades += 1
+        self.total_profit += gain
+        self.total_gain += gain
+        self.num_gain += 1
+
+    def get_results(self):
+        win_rate = self.num_gain / self.closed_trades
+        loss_rate = self.num_loss / self.closed_trades
+
+        avg_gain = self.total_gain / self.num_gain
+        avg_loss = self.total_loss / self.num_loss
+        expectancy = (win_rate * avg_gain) - (loss_rate * avg_loss)
+
+        profit_factor = self.total_gain / self.total_loss
+
+        max_drawdown = 0
+        sharpe_ratio = 0
+        return win_rate, expectancy, profit_factor, max_drawdown, sharpe_ratio
+
 
 if __name__ == "__main__":
     env = Environment("train", 2)

24e59fb631263eea48550280b911a86d02f410ae
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-04-14 18:24:15 +0200

    [ADD] init_state market_data format test

---
+++ test/test_init_state.py
@@ -0,0 +1,16 @@
+import unittest
+from src import init_state as init
+
+PARAMS = ['train', True, True, True]
+
+class TestInitEnvironment(unittest.TestCase):
+
+    def test_market_data_has_correct_dimensions(self):
+        market_data = init.get_market_data(*PARAMS)
+
+        expected_width = 9
+        actual_height, actual_width = market_data.shape
+
+        self.assertEqual(actual_width, expected_width,
+                         f"Expected {expected_width} columns, but got {actual_width}")
+        self.assertGreater(actual_height, 0, "Market data is empty!")

8fc63d8320128b7684fff35c1cbb5ffbcc519b99
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-04-18 01:52:13 +0200

    [CREATE] live_test.py

---
+++ src/live_test.py
@@ -0,0 +1,13 @@
+# todo Load trained model from memory
+
+# Loop
+# todo Get candlesticks/data from MetaTrader
+
+# todo Translate in-data to format used in training (normalize, etc)
+
+# todo Let model decide action to take
+
+# todo Send order via MetaTrader
+
+# After loop
+# todo Use history_deals_get or history_orders_get to extract results (not really sure if they work)

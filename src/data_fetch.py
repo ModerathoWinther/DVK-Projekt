04915a06de7c3ecb1da50ecd173fe447a2f56cc2
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-04-17 16:33:21 +0200

    [FIX] data path outside of project dir

--- src/data_fetch.py
+++ src/data_fetch.py
@@ -8,21 +8,20 @@ import os
 # Constant configuration
 TIMEFRAME = mt5.TIMEFRAME_M15
 SYMBOL = "XAUUSD"
-OUTPUT_DIR = "../data/raw"
 TIMEZONE = pytz.timezone("Etc/UTC")
 DATE_FROM = datetime(2016, 1, 1, tzinfo=TIMEZONE)
 DATE_TO = datetime(2025, 12, 31, hour=23, tzinfo=TIMEZONE)
-FILE_NAME = f"data/raw/{SYMBOL}_{TIMEFRAME}M_{DATE_FROM.date()}_{DATE_TO.date()}.csv"
-SPLITS = ["train", "val", "test"]
-#
 
-# Change to ROOT dir
+# Change to project dir
 os.chdir("..")
+
+OUTPUT_DIR = os.path.abspath("data/raw")
+FILE_NAME = os.path.join(OUTPUT_DIR, f"{SYMBOL}_{TIMEFRAME}M_{DATE_FROM.date()}_{DATE_TO.date()}.csv")
+SPLITS = ["train", "val", "test"]
 
 
 # Downloads data via MetaTrader 5 and exports to .csv
 def fetch_and_save_data():
-
     if not mt5.initialize():
         print(f"MT5 initialize() failed, error code = {mt5.last_error()}")
         return
@@ -59,9 +58,9 @@ def fetch_and_save_data():
     print(f"Successfully saved {actual_rows} rows to {FILE_NAME}")
     print(f"Data range: {final_df['date'].min()} to {final_df['date'].max()}")
 
+
 # Parti
 def partition_fetched_data():
-
     if not os.path.isfile(FILE_NAME):
         print(f"Error: Raw data file not found with file name {FILE_NAME}.")
         return
@@ -72,19 +71,17 @@ def partition_fetched_data():
     train_end = pd.Timestamp('2022-12-31 23:59:59')
     val_end = pd.Timestamp('2023-12-31 23:59:59')
 
-
     train_df = df[df['date'] <= train_end].copy()
-    val_df   = df[(df['date'] > train_end) & (df['date'] <= val_end)].copy()
-    test_df  = df[df['date'] > val_end].copy()
+    val_df = df[(df['date'] > train_end) & (df['date'] <= val_end)].copy()
+    test_df = df[df['date'] > val_end].copy()
 
     for split in SPLITS:
         dirs = os.path.join(OUTPUT_DIR, split)
         os.makedirs(dirs, exist_ok=True)
 
-
     train_path = f"{OUTPUT_DIR}/train/raw_train.csv"
-    val_path   = f"{OUTPUT_DIR}/val/raw_val.csv"
-    test_path  = f"{OUTPUT_DIR}/test/raw_test.csv"
+    val_path = f"{OUTPUT_DIR}/val/raw_val.csv"
+    test_path = f"{OUTPUT_DIR}/test/raw_test.csv"
     train_df.to_csv(train_path, index=False)
     val_df.to_csv(val_path, index=False)
     test_df.to_csv(test_path, index=False)
@@ -92,14 +89,15 @@ def partition_fetched_data():
     print(f"Saved splits to {OUTPUT_DIR}/X_path")
     return train_path, val_path, test_path
 
-def run():
 
+def run():
     if os.path.isfile(FILE_NAME):
         print("Raw files already exist.")
         return
 
     fetch_and_save_data()
     partition_fetched_data()
+
 
 if __name__ == "__main__":
     run()

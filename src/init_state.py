da14f59547a112fea53cf06881f464be8562abe4
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-04-23 22:24:11 +0200

    [UPDATE] now always returns all indicators

--- src/init_state.py
+++ src/init_state.py
@@ -6,23 +6,18 @@ PRICE_DIR = dp.NORMAL_DIR
 INDICATOR_DIR = dp.INDICATOR_DIR
 STATIONARY_DIR = dp.STATIONARY_DIR
 
-def get_input_data(split, atr=False, macd=False, rsi=False):
+def get_input_data(split):
     price = pd.read_csv(f"{STATIONARY_DIR}/{split}.csv",
                         index_col="date", parse_dates=["date"])
 
-    print(f'price.columns = {price.columns}')
-    frames = [price]
-    if atr:
-        frames.append(pd.read_csv(f"{INDICATOR_DIR}/{split}/atr.csv",
-                                  index_col="date", parse_dates=["date"]))
-    if macd:
-        frames.append(pd.read_csv(f"{INDICATOR_DIR}/{split}/macd.csv",
-                                  index_col="date", parse_dates=["date"]))
-    if rsi:
-        frames.append(pd.read_csv(f"{INDICATOR_DIR}/{split}/rsi.csv",
-                                  index_col="date", parse_dates=["date"]))
+    frames = [price, pd.read_csv(f"{INDICATOR_DIR}/{split}/atr.csv",
+                                 index_col="date", parse_dates=["date"]),
+              pd.read_csv(f"{INDICATOR_DIR}/{split}/macd.csv",
+                          index_col="date", parse_dates=["date"]),
+              pd.read_csv(f"{INDICATOR_DIR}/{split}/rsi.csv",
+                          index_col="date", parse_dates=["date"])]
+
     input_data = pd.concat(frames, axis=1).dropna()
-    print(f'input_data.columns = {input_data.columns}')
 
     return input_data.to_numpy()
 
@@ -32,6 +27,4 @@ def get_prices(split):
     return price.to_numpy()
 
 def run(**params):
-    input_data = get_input_data(params.get('split'), atr=params.get('atr'), macd=params.get('macd'), rsi=params.get('rsi'))
-    prices = get_prices(params.get('split'))
-    return input_data, prices
+    return get_input_data(params.get('split')), get_prices(params.get('split'))

c986dbb9cb097bf6b1378b20c3beb1249fb0999e
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-04-28 14:19:20 +0200

    [FIX] typo

--- src/live_test.py
+++ src/live_test.py
@@ -19,7 +19,7 @@ import indicators as ind
 
 DEVICE = 'cpu'
 RESULTS_DIR = 'results'
-WARMuP_BARS = dp.WARMUP_ROWS + 1
+WARMUP_BARS = dp.WARMUP_ROWS + 1
 
 class LiveTest:
 
@@ -83,7 +83,7 @@ class LiveTest:
 
 
     def get_market_data(self) -> pd.DataFrame:
-        rates = mt5.copy_rates_from_pos("XAUUSD", mt5.TIMEFRAME_M15, 0, WARMuP_BARS)
+        rates = mt5.copy_rates_from_pos("XAUUSD", mt5.TIMEFRAME_M15, 0, WARMUP_BARS)
         df = pd.DataFrame(rates)
         df = df.rename(columns={'tick_volume': 'volume', 'time': 'date'})
         df['date'] = pd.to_datetime(df['date'], unit='s')
@@ -213,6 +213,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     # mt5.initialize(args.mt5_details)
     mt5.initialize()
+    print(mt5.symbol_info("XAUUSD"))
     midas = LiveTest(params=args.hyperparameters)
     midas.run()
 

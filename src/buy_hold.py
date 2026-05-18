8c2feeb5d26e6feba94386a132afdc6a7e6206d5
Simon Jonsson <166023105+Nomis778@users.noreply.github.com>
2026-05-18 16:22:39 +0200

    [UPDATE] buy hold logic

--- src/buy_hold.py
+++ src/buy_hold.py
@@ -11,21 +11,23 @@ with open('hyperparameters.yml', 'r') as file:
 env_make_params = hyperparameters.get('env_make_params', {})
 env_id = hyperparameters.get('env_id')
 env = TradingEnvironment(env_make_params)
-equity = 0
-for i in range(50):
+
+num_episodes = hyperparameters['max_episodes']  # or however many you want to run
+counter = 1
+
+for counter in range(num_episodes):
     for i in range(env.num_trades):
         env._process_action(BUY_HOLD_ACTION)
 
     _, _, terminated, _, _ = env.step(HOLD_ACTION.index)
-    reward = 0
     while not terminated:
-        _, equity, terminated, _, _ = env.step(HOLD_ACTION.index)
+        _, _, terminated, _, _ = env.step(HOLD_ACTION.index)
 
     env.reset()
-    if equity > 0: env.equity_curve.append(equity)
-    ep_stats = env.get_episode_stats()
-    equity = 0
-    os.makedirs(f'results/backtest/{env_id}', exist_ok=True)
-    df = pd.DataFrame(ep_stats)
-    df['id'] = env_id
-    df.to_csv(f'results/backtest/{env_id}/{env_id}.csv', index=False)
+    counter += 1
+
+# Save to CSV
+os.makedirs(f'results/backtest/{env_id}', exist_ok=True)
+df = pd.DataFrame(env.get_episode_stats())
+df['id'] = env_id
+df.to_csv(f'results/backtest/{env_id}/{env_id}.csv', index=False)
